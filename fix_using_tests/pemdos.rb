e3eec0ff6285d47443236f2a7ec4ba72f20779f0
Benjamin Stein <whereyouben@gmail.com>
2020-10-12 02:06:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,10 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    (9 * "s") + string
+    10 * "s" + string
+    binding.pry
   else
     string
   end
